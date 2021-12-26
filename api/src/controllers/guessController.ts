65682dd97bb963380f81ed428cfa043766caad2a
Jakob Jónasson <jakob.jonasson@gmail.com>
2021-12-26 23:38:48 +0000

    Add mongo test server

--- api/src/controllers/guessController.ts
+++ api/src/controllers/guessController.ts
@@ -1,15 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import { HttpStatus } from '../utils/httpUtils';
-
-interface IGuess {
-  userId: String;
-  name: String;
-}
-
-// TODO: Read from DB
-const guesses = Array<IGuess>();
+import * as guessesModel from '../models/guessesModel';
 
 const getGuesses = async (req: Request, res: Response) => {
+  const guesses = await guessesModel.getGuesses();
   return res.status(HttpStatus.OK).json({
     results: guesses
   });
@@ -17,9 +11,9 @@ const getGuesses = async (req: Request, res: Response) => {
 
 // TODO: Add pagination
 const addGuess = async (req: Request, res: Response, next: NextFunction) => {
-  const inputGuess = req.body as IGuess;
-  guesses.push(inputGuess);
-  return res.status(HttpStatus.OK).json(inputGuess);
+  const inputGuess = req.body as guessesModel.IGuess;
+  const createdGuess = await guessesModel.createGuess(inputGuess);
+  return res.status(HttpStatus.OK).json(createdGuess);
 };
 
 export default { addGuess, getGuesses };
