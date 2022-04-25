62af09d470e4a0e4908d2f5fe93f287f81482cef
Jakob Jónasson <jakob.jonasson@gmail.com>
2022-04-25 19:24:03 +0000

    Move from docker-compose to Kubernetes (#19)

---
+++ k8s/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd k8s
+
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo-configmap.yaml
+kubectl apply -f mongo.yaml
+kubectl apply -f mongo-express.yaml
+kubectl apply -f api.yaml
+kubectl apply -f frontend.yaml
+kubectl apply -f ingress-service.yaml
