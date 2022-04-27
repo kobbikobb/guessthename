2b593cf0e8061ff6a410c2fb51d696bb80d928c1
Jakob Jónasson <jakob.jonasson@gmail.com>
2022-04-27 22:47:18 +0000

    Add pvc for mongo (#21)

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -4,6 +4,7 @@ cd k8s
 
 kubectl apply -f mongo-secret.yaml
 kubectl apply -f mongo-configmap.yaml
+kubectl apply -f mongo-pvc.yaml
 kubectl apply -f mongo.yaml
 kubectl apply -f mongo-express.yaml
 kubectl apply -f api.yaml
