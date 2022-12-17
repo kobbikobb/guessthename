eda617d1a5969a7e1d1eb026ddc184ad78c54e17
Jakob Jónasson <jakob.jonasson@gmail.com>
2022-12-17 23:03:13 +0000

    update images id (#66)

--- scripts/build-api.sh
+++ scripts/build-api.sh
@@ -7,6 +7,6 @@ echo "Build guessthename_api #$COMMIT and latest"
 cd api
 docker image build -f "Dockerfile" . \
   --build-arg "app_name=guessthename_api" \
-  -t "kobbikobb/guessthename_api:${COMMIT}"
+  -t "kobbikobb/guessthename_api:${COMMIT}" \
   -t "kobbikobb/guessthename_api:latest"
 cd ..
--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -7,6 +7,6 @@ echo "Build guessthename_frontend #$COMMIT and latest"
 cd frontend
 docker image build -f "Dockerfile" . \
   --build-arg "app_name=guessthename_frontend" \
-  -t "kobbikobb/guessthename_frontend:${COMMIT}"
+  -t "kobbikobb/guessthename_frontend:${COMMIT}" \
   -t "kobbikobb/guessthename_frontend:latest"
 cd ..
