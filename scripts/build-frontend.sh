59402eb6227b026ea7f4407f60b63cc92bb60f0a
Jakob Jónasson <jakob.jonasson@gmail.com>
2023-01-03 18:20:27 +0000

    Build and push to ecr

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-COMMIT=$(git rev-parse --verify HEAD)
+source scripts/variables.sh
 
 echo "Build guessthename_frontend #$COMMIT and latest"
 
 cd frontend
 docker image build -f "Dockerfile" . \
   --build-arg "app_name=guessthename_frontend" \
-  -t "kobbikobb/guessthename_frontend:${COMMIT}" \
-  -t "kobbikobb/guessthename_frontend:latest"
+  -t "$ECR_BASE_PATH/guessthename_frontend:${COMMIT}" \
+  -t "$ECR_BASE_PATH/guessthename_frontend:latest"
 cd ..
---
+++ scripts/variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+COMMIT=$(git rev-parse --verify HEAD)
+ECR_BASE_PATH=public.ecr.aws/q3b8h4t9
