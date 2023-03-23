f044330f847cbf8f12b1e84e0db248f7ea488a49
Jakob Jónasson <jakob.jonasson@gmail.com>
2023-03-23 14:03:25 +0000

    Add husky (#95)

---
+++ api/src/utils/errors.ts
@@ -0,0 +1,14 @@
+import { HttpStatus } from '../utils/httpUtils';
+
+export const badRequest = (message: string): HttpError => {
+  return new HttpError(HttpStatus.BAD_REQUEST, message);
+};
+
+export class HttpError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
