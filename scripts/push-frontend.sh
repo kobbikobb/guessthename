27d3664894635ab8309fa1499c357c06d464eb2b
Jakob Jónasson <jakob.jonasson@gmail.com>
2023-04-20 21:33:06 +0000

    Add local build capabilities (#113)

--- scripts/push-frontend.sh
+++ scripts/push-frontend.sh
@@ -4,5 +4,5 @@ source scripts/variables.sh
 
 echo "Pushing guessthename_frontend image and latest #$COMMIT"
 
-docker push $ECR_BASE_PATH/guessthename_frontend:$COMMIT
-docker push $ECR_BASE_PATH/guessthename_frontend:latest
+docker push $REGISTRY_BASE_PATH/guessthename_frontend:$COMMIT
+docker push $REGISTRY_BASE_PATH/guessthename_frontend:latest
--- scripts/push-worker.sh
+++ scripts/push-worker.sh
@@ -4,5 +4,5 @@ source scripts/variables.sh
 
 echo "Pushing guessthename_worker and latest #$COMMIT"
 
-docker push $ECR_BASE_PATH/guessthename_worker:$COMMIT
-docker push $ECR_BASE_PATH/guessthename_worker:latest
+docker push $REGISTRY_BASE_PATH/guessthename_worker:$COMMIT
+docker push $REGISTRY_BASE_PATH/guessthename_worker:latest
