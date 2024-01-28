611c6ab9d9e1177d3aaec5afe0c6828568963fad
Jakob Jónasson <jakobjo@tempo.io>
2024-01-28 21:35:36 +0000

    📚Improve documentation for Rancher Desktop

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -6,7 +6,7 @@ COMMIT=$(git rev-parse --verify HEAD)
 echo "Commit #$COMMIT"
 
 if [ -z "${REGISTRY_BASE_PATH+x}" ]; then
-  export REGISTRY_BASE_PATH="public.ecr.aws/q3b8h4t9"
+  export REGISTRY_BASE_PATH="public.ecr.aws\/q3b8h4t9"
 else
   export REGISTRY_BASE_PATH="$REGISTRY_BASE_PATH"
 fi
