bac1f107a11bb4fb16799101bbc2bf03a76623d3
Jakob Jónasson <jakobjo@tempo.io>
2024-09-28 09:50:38 +0000

    Update axios

--- docker/run-api.sh
+++ docker/run-api.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-COMMIT=$(git rev-parse --verify HEAD)
+source scripts/variables.sh
 
 docker run -d \
 -p 3000:3000 \
 -e MONGO_DB_URI="mongodb://guess-mongo/api" \
 --network=guess-network \
-kobbikobb/guessthename_api:$COMMIT
+"$REGISTRY_BASE_PATH/guessthename_api:${COMMIT}" 
