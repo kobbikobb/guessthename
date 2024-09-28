329062d19df4dd00bb415022863f22b54a1004c7
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-09-28 13:57:53 +0000

    Fake monog setup (#166)

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    globalSetup: "<rootDir>/src/__helper__/fakeMongoGlobalSetup.ts",
+    globalTeardown: "<rootDir>/src/__helper__/fakeMongoGlobalTeardown.ts",
+    setupFilesAfterEnv: [
+        "<rootDir>/src/__helper__/fakeMongoUnitTestSetup.ts"
+    ]
 };
--- api/src/controllers/__tests__/nameTargetController.spec.ts
+++ api/src/controllers/__tests__/nameTargetController.spec.ts
@@ -1,17 +1,8 @@
 import request from 'supertest';
-import {
-  connect,
-  cleanData,
-  disconnect
-} from '../../__helper__/mongodb.memory.test.helper';
 import app from '../../app';
 import { HttpStatus } from '../../utils/httpUtils';
 
 describe('Name Target Controller', () => {
-  beforeAll(connect);
-  beforeEach(cleanData);
-  afterAll(disconnect);
-
   it('should create a name target', async () => {
     const response = await request(app).post('/name-target').send({
       userId: 'the-user-id',
@@ -62,33 +53,26 @@ describe('Name Target Controller', () => {
     expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
   });
 
-  it('should fetch an empty list of name targets', async () => {
-    const response = await request(app).get('/name-target');
-
-    expect(response.statusCode).toBe(HttpStatus.OK);
-    expect(response.body).toEqual({ results: [] });
-  });
-
   it('should fetch a list of one name target', async () => {
     const createdResponse = await request(app).post('/name-target').send({
       userId: 'the-user-id',
       title: 'A title',
       name: 'A secret name to guess'
     });
-    const created = createdResponse.body;
+    const { id, createdAt } = createdResponse.body;
 
     const response = await request(app).get('/name-target');
 
     expect(response.statusCode).toBe(HttpStatus.OK);
     expect(response.body).toEqual({
-      results: [
-        {
+      results: expect.arrayContaining([
+        expect.objectContaining({
           userId: 'the-user-id',
           title: 'A title',
-          id: created.id,
-          createdAt: created.createdAt
-        }
-      ]
+          id,
+          createdAt
+        })
+      ])
     });
   });
 });
