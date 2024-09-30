1e102b18819c13fcd27f1ab7fcd0562bc4985d4b
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-09-30 18:51:54 +0000

    Eslint upgrade (#194)

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,29 +2,29 @@ import { Consumer } from 'sqs-consumer';
 import { tryParseJson } from './messageHelper';
 
 const queueUrl =
-  'https://sqs.us-east-1.amazonaws.com/779377485039/my-worker-queue';
+    'https://sqs.us-east-1.amazonaws.com/779377485039/my-worker-queue';
 const region = 'us-east-1';
 
 const app = Consumer.create({
-  queueUrl,
-  region,
-  handleMessage: async (message) => {
-    const action = tryParseJson(message.Body);
-    if (action != undefined) {
-      console.log(`Received an action ${action.name}`);
-    }
+    queueUrl,
+    region,
+    handleMessage: async (message) => {
+        const action = tryParseJson(message.Body);
+        if (action != undefined) {
+            console.log(`Received an action ${action.name}`);
+        }
 
-    console.log(`Message handled: ${message.Body}`);
-  }
+        console.log(`Message handled: ${message.Body}`);
+    }
 });
 
 app.on('error', (err) => {
-  console.error(`Unhanlded exception: ${err.message}`);
-  process.exit();
+    console.error(`Unhanlded exception: ${err.message}`);
+    process.exit();
 });
 
 app.on('processing_error', (err) => {
-  console.error(`Processing error: ${err.message}`);
+    console.error(`Processing error: ${err.message}`);
 });
 
 console.log('Worker is starting!');
