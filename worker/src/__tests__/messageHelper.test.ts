0732e1e89e2493d8a55c883315529ed6da4571e3
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-09-30 18:50:43 +0000

    Eslint upgrade

--- worker/src/__tests__/messageHelper.test.ts
+++ worker/src/__tests__/messageHelper.test.ts
@@ -1,16 +1,16 @@
 import { tryParseJson } from '../messageHelper';
 
 describe('messageHelper', () => {
-  it('should parse undefined json', () => {
-    expect(tryParseJson(undefined)).toEqual(undefined);
-  });
+    it('should parse undefined json', () => {
+        expect(tryParseJson(undefined)).toEqual(undefined);
+    });
 
-  it('should parse invalid json', () => {
-    expect(tryParseJson('invalid')).toEqual(undefined);
-  });
+    it('should parse invalid json', () => {
+        expect(tryParseJson('invalid')).toEqual(undefined);
+    });
 
-  it('should parse json', () => {
-    const jsonStrong = '{"action":"value"}';
-    expect(tryParseJson(jsonStrong)).toEqual({ action: 'value' });
-  });
+    it('should parse json', () => {
+        const jsonStrong = '{"action":"value"}';
+        expect(tryParseJson(jsonStrong)).toEqual({ action: 'value' });
+    });
 });
--- worker/src/messageHelper.ts
+++ worker/src/messageHelper.ts
@@ -1,10 +1,10 @@
 export const tryParseJson = (jsonString: string | undefined) => {
-  if (jsonString === undefined) {
-    return undefined;
-  }
-  try {
-    return JSON.parse(jsonString);
-  } catch {
-    return undefined;
-  }
+    if (jsonString === undefined) {
+        return undefined;
+    }
+    try {
+        return JSON.parse(jsonString);
+    } catch {
+        return undefined;
+    }
 };
