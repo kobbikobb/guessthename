5b93c5adabff53351c1f08fe6670e75b6f6e3689
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-12-26 21:21:41 +0000

    Update eslint etc (#340)

--- api/src/routes/guessRoutes.ts
+++ api/src/routes/guessRoutes.ts
@@ -3,9 +3,8 @@ import controller from '../controllers/guessController';
 
 const router = express.Router();
 
-// eslint-disable-next-line
 router.post('/guess', controller.postGuess);
-// eslint-disable-next-line
+
 router.get('/guess', controller.getGuesses);
 
 export default router;
