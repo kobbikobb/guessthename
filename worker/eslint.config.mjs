9082c331d8b869c9303a6a82855110d0dd70b04b
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-12-26 11:09:18 +0000

    Update prettier for worker (#337)

--- worker/eslint.config.mjs
+++ worker/eslint.config.mjs
@@ -1,20 +1,35 @@
-import globals from "globals";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import eslintPluginPromise from "eslint-plugin-promise";
+import importPlugin from 'eslint-plugin-import';
 
-export default [
-  {
-    files: ["**/*.{ts}"],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    }
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
-  eslintPluginPromise.configs['flat/recommended'],
-];
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    eslintPluginPrettier,
+    eslintPluginPromise.configs['flat/recommended'],
+    importPlugin.flatConfigs.recommended,
+    {
+        settings: {
+            'import/resolver': {
+                node: {
+                    extensions: ['.ts'],
+                    paths: ['.'],
+                },
+                typescript: {},
+            }
+        },
+        rules: {
+            'import/order': [
+                'warn',
+                {
+                    groups: ['builtin', 'external', 'internal'],
+                    'newlines-between': 'never',
+                },
+            ],
+        }
+    },
+);
